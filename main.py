1486790fd45ca34132f5afb06f4943927a0cb9c4
Thắng Lê Trần <letranthang0606@gmail.com>
2024-06-18 17:09:08 +0700

    first commit

--- main.py
+++ main.py
@@ -97,9 +97,9 @@ def main(frames, scenes, bounces, ball_track, homography_matrices, kps_court, pe
                 # draw bounce in minimap
                 if i in bounces and inv_mat is not None:
                     ball_point = ball_track[i]
-                    ball_point = np.array(ball_point, dtype=np.float32).reshape(1, 1, 2)
                     img_res = cv2.circle(img_res, (int(ball_point[0]), int(ball_point[1])),
-                                        radius=0, color=(255, 0, 0), thickness=50)
+                                        radius=0, color=(255, 0, 0), thickness=5)
+                    ball_point = np.array(ball_point, dtype=np.float32).reshape(1, 1, 2)
                     ball_point = cv2.perspectiveTransform(ball_point, inv_mat)
                     court_img = cv2.circle(court_img, (int(ball_point[0, 0, 0]), int(ball_point[0, 0, 1])),
                                                        radius=0, color=(0, 255, 255), thickness=50)
