1699a93e2b2659da2a73a51e1eb8d507feb9401f
Thắng Lê Trần <letranthang0606@gmail.com>
2024-06-18 18:17:56 +0700

    first commit

--- court_detection_net.py
+++ court_detection_net.py
@@ -17,41 +17,72 @@ class CourtDetectorNet():
     def infer_model(self, frames, transform):
         output_width = 640
         output_height = 360
-        scale = 3
+        scale = frames[0].shape[0] / self.height
         
         kps_res = []
         matrixes_res = []
-        for num in tqdm(range(2, len(frames), 3)):
-            img = frames[num]
-            img_prev = frames[num-1]
-            img_preprev = frames[num-2]
-            image_list = [img_preprev, img_prev, img]
-            imgs = torch.cat([transform(image = img)['image'] for img in image_list])
+        # for num in tqdm(range(2, len(frames), 3)):
+        #     img = frames[num]
+        #     img_prev = frames[num-1]
+        #     img_preprev = frames[num-2]
+        #     image_list = [img_preprev, img_prev, img]
+        #     imgs = torch.cat([transform(image = img)['image'] for img in image_list])
             
-            inp = torch.unsqueeze(imgs, 0)
+        #     inp = torch.unsqueeze(imgs, 0)
 
-            out = self.model(inp.to(self.device))
+        #     out = self.model(inp.to(self.device))
+        #     pred = F.sigmoid(out).detach().cpu().numpy()
+
+            
+        #     for i in range(3):
+        #         points = []
+        #         for kps_num in range(14):
+        #             x_pred, y_pred = self._detect_blob_concomp(pred[0][i * 15 + kps_num])
+        #             if x_pred is not None:
+        #                 if kps_num not in [8, 12, 9]:
+        #                     x_pred, y_pred = refine_kps(image_list[i], int(y_pred), int(x_pred), crop_size=40)
+        #                 points.append((x_pred, y_pred))                
+        #             else:
+        #                 points.append(None)
+
+        #         matrix_trans = get_trans_matrix(points) 
+        #         points = None
+        #         if matrix_trans is not None:
+        #             points = cv2.perspectiveTransform(refer_kps, matrix_trans)
+        #             matrix_trans = cv2.invert(matrix_trans)[1]
+        #         kps_res.append(points)
+        #         matrixes_res.append(matrix_trans)
+        for num_frame, image in enumerate(tqdm(frames)):
+            img = cv2.resize(image, (output_width, output_height))
+            inp = (img.astype(np.float32) / 255.)
+            inp = torch.tensor(np.rollaxis(inp, 2, 0))
+            inp = inp.unsqueeze(0)
+
+            out = self.model(inp.float().to(self.device))[0]
             pred = F.sigmoid(out).detach().cpu().numpy()
 
-            
-            for i in range(3):
-                points = []
-                for kps_num in range(14):
-                    x_pred, y_pred = self._detect_blob_concomp(pred[0][i * 15 + kps_num])
-                    if x_pred is not None:
-                        if kps_num not in [8, 12, 9]:
-                            x_pred, y_pred = refine_kps(image_list[i], int(y_pred), int(x_pred), crop_size=40)
-                        points.append((x_pred, y_pred))                
-                    else:
-                        points.append(None)
+            points = []
+            for kps_num in range(14):
+                heatmap = (pred[kps_num]*255).astype(np.uint8)
+                ret, heatmap = cv2.threshold(heatmap, 170, 255, cv2.THRESH_BINARY)
+                circles = cv2.HoughCircles(heatmap, cv2.HOUGH_GRADIENT, dp=1, minDist=20, param1=50, param2=2,
+                                           minRadius=10, maxRadius=25)
+                if circles is not None:
+                    x_pred = circles[0][0][0]*scale
+                    y_pred = circles[0][0][1]*scale
+                    if kps_num not in [8, 12, 9]:
+                        x_pred, y_pred = refine_kps(image, int(y_pred), int(x_pred), crop_size=40)
+                    points.append((x_pred, y_pred))                
+                else:
+                    points.append(None)
 
-                matrix_trans = get_trans_matrix(points) 
-                points = None
-                if matrix_trans is not None:
-                    points = cv2.perspectiveTransform(refer_kps, matrix_trans)
-                    matrix_trans = cv2.invert(matrix_trans)[1]
-                kps_res.append(points)
-                matrixes_res.append(matrix_trans)
+            matrix_trans = get_trans_matrix(points) 
+            points = None
+            if matrix_trans is not None:
+                points = cv2.perspectiveTransform(refer_kps, matrix_trans)
+                matrix_trans = cv2.invert(matrix_trans)[1]
+            kps_res.append(points)
+            matrixes_res.append(matrix_trans)
         return matrixes_res, kps_res    
 
     def _detect_blob_concomp(self, hm, _score_threshold = 0.5, _use_hm_weight = False, scale = 1080/288):
